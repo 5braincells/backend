72ec41e8b319777b91f1bcd4b2f0d22c68893e5e
Vlad Cainamisir <vlad.cainamisir@gmail.com>
2021-03-11 21:43:05 +0200

    login

--- user/userHandlers.js
+++ user/userHandlers.js
@@ -1,32 +1,58 @@
-const e = require("express");
-const md5 = require("md5");
 const sha256 = require("sha256");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv").config();
 const { ObjectID } = require("mongodb");
 
-function registerUser(db, req, res) {
-  let userData = req.body.userData;
+const privateKey = process.env.PRIVATE_KEY;
 
-  db.collection("Users").insertOne(
-    {
-      email: userData.email,
-      password: sha256.x2(userData.password),
-      firstName: userData.firstName,
-      lastName: userData.lastName,
-      grade: userData.grade,
-    },
-    (err, data) => {
-      if (err) {
-        res.status(401).send({
-          reason: "Sunteti deja inregistrat cu acest email in aplicatie!",
-        });
-      } else {
-        res.status(200).send({
-          response:
-            "V-ati inregistrat cu succes! Va rugam verificati mailul pentru confirmare",
-        });
-      }
-    }
-  );
+function registerUser(db, req, res) {
+    let userData = req.body.userData;
+    db.collection("Users").findOne({ email: userData.email }, (err, data) => {
+        if (err) res.status(500).send({ error: "DB did not respond" });
+        else if (data == null) {
+            db.collection("Users").insertOne(
+                {
+                    email: userData.email,
+                    password: sha256.x2(userData.password),
+                    firstName: userData.firstName,
+                    lastName: userData.lastName,
+                    grade: userData.grade,
+                },
+                (err, data) => {
+                    if (err) {
+                        res.status(401).send({
+                            reason:
+                                "Sunteti deja inregistrat cu acest email in aplicatie!",
+                        });
+                    } else {
+                        res.status(200).send({
+                            response:
+                                "V-ati inregistrat cu succes! Va rugam verificati mailul pentru confirmare",
+                        });
+                    }
+                }
+            );
+        } else {
+            res.status(401).send({
+                reason: "Sunteti deja inregistrat cu acest email in aplicatie!",
+            });
+        }
+    });
+}
+
+function loginUser(db, req, res) {
+    let userData = req.body.userData;
+    db.collection("Users").findOne({ email: userData.email }, (err, data) => {
+        if (err) res.status(401).send("Nu aveti cont in aplicatie");
+        else if (sha256.x2(userData.password) === data.password) {
+            var token = jwt.sign({ userID: ObjectID(data._id) }, privateKey);
+            res.status(200).send({
+                response: "V-ati autentificat cu succes!",
+                token,
+            });
+        }
+    });
 }
 
 exports.registerUser = registerUser;
+exports.loginUser = loginUser;
