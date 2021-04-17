7ffa4026094a90b5cf00e9320a2cb12e81f7298f
Vlad Cainamisir <vlad.cainamisir@gmail.com>
2021-04-17 21:42:57 +0300

    handle disconnect for webrtc

--- index.js
+++ index.js
@@ -130,6 +130,7 @@ async function main() {
                 room = room.filter((id) => id !== socket.id);
                 users[roomID] = room;
             }
+            socket.broadcast.emit("user left", socket.id);
         });
     });
 }
