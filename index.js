f470b29b9c7c90bec18cfe9ea298a5762da59939
Vlad <vlad.cainamisir@gmail.com>
2021-04-24 22:42:16 +0300

    email conformation

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const messageHandlers = require("./messaging/messageHandlers.js");
 const path = require("path");
 const multer = require("multer");
 var cors = require("cors");
+var nodemailer = require("nodemailer");
 const NodeCache = require("node-cache");
 const socket = require("socket.io");
 const users = {};
@@ -13,134 +14,143 @@ const users = {};
 const socketToRoom = {};
 
 async function main() {
-    //Database setup
-    const uri = process.env.DB_URI;
-    let db;
-    const client = new MongoClient(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+  //Database setup
+  const uri = process.env.DB_URI;
+  let db;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  await client.connect();
+
+  db = client.db("StudyRooms");
+  const cache = new NodeCache({ stdTTL: 10000, checkperiod: 12000 });
+
+  ///Images folder setup
+  var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "files/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + file.originalname); //Appending .jpg
+    },
+  });
+  const upload = multer({
+    dest: "files",
+    limits: {
+      fileSize: 32000000,
+    },
+    storage: storage,
+  });
+
+  var mail = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "studyroomsnoreply@gmail.com",
+      pass: "RADUSEFULLADESIGN",
+    },
+  });
+
+  //Routes
+
+  const app = express();
+  app.use("/files", express.static(path.join(__dirname + "/files")));
+
+  app.use(cors());
+  app.use(express.json());
+  app.post("/api/register", (req, res) => {
+    userHandlers.registerUser(db, req, res, mail);
+  });
+
+  app.post("/api/login", (req, res) => {
+    userHandlers.loginUser(db, req, res);
+  });
+
+  app.post("/api/logout", (req, res) => {
+    userHandlers.logoutUser(db, req, res);
+  });
+
+  app.get("/api/profile/:id", (req, res) => {
+    userHandlers.getProfile(db, req, res, cache);
+  });
+  app.post("/api/changeProfile", (req, res) => {
+    userHandlers.changeProfile(db, req, res);
+  });
+  app.post("/api/sendMessage", (req, res) => {
+    messageHandlers.sendMessage(db, req, res);
+  });
+
+  app.post("/api/sendPicture", upload.single("image"), (req, res) => {
+    messageHandlers.sendPicture(db, req, res);
+  });
+  app.post("/api/sendFile", upload.single("file"), (req, res) => {
+    messageHandlers.sendFile(db, req, res);
+  });
+  app.post("/api/getMessages", (req, res) => {
+    messageHandlers.getMessages(db, req, res);
+  });
+  app.post("/api/deleteMessage", (req, res) => {
+    messageHandlers.deleteMessage(db, req, res);
+  });
+  app.get("/api/generalData", (req, res) => {
+    userHandlers.getGeneralData(db, req, res, cache);
+  });
+  app.get("/api/confirm/:id", (req, res) => {
+    userHandlers.confirmAccount(db, req, res);
+  });
+  //Start server
+  const server = app.listen(8080, () => {
+    console.log("started on 8080");
+  });
+  const io = socket(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    socket.on("join room", (roomID) => {
+      if (users[roomID]) {
+        const length = users[roomID].length;
+        if (length === 10) {
+          socket.emit("room full");
+          return;
+        }
+        users[roomID].push(socket.id);
+      } else {
+        users[roomID] = [socket.id];
+      }
+      socketToRoom[socket.id] = roomID;
+      const usersInThisRoom = users[roomID].filter((id) => id !== socket.id);
+
+      socket.emit("all users", usersInThisRoom);
     });
 
-    await client.connect();
-
-    db = client.db("StudyRooms");
-    const cache = new NodeCache({ stdTTL: 10000, checkperiod: 12000 });
-
-    ///Images folder setup
-    var storage = multer.diskStorage({
-        destination: function (req, file, cb) {
-            cb(null, "files/");
-        },
-        filename: function (req, file, cb) {
-            cb(null, Date.now() + file.originalname); //Appending .jpg
-        },
-    });
-    const upload = multer({
-        dest: "files",
-        limits: {
-            fileSize: 32000000,
-        },
-        storage: storage,
+    socket.on("sending signal", (payload) => {
+      io.to(payload.userToSignal).emit("user joined", {
+        signal: payload.signal,
+        callerID: payload.callerID,
+      });
     });
 
-    //Routes
-
-    const app = express();
-    app.use("/files", express.static(path.join(__dirname + "/files")));
-
-    app.use(cors());
-    app.use(express.json());
-    app.post("/api/register", (req, res) => {
-        userHandlers.registerUser(db, req, res);
+    socket.on("returning signal", (payload) => {
+      io.to(payload.callerID).emit("receiving returned signal", {
+        signal: payload.signal,
+        id: socket.id,
+      });
     });
 
-    app.post("/api/login", (req, res) => {
-        userHandlers.loginUser(db, req, res);
+    socket.on("disconnect", () => {
+      const roomID = socketToRoom[socket.id];
+      let room = users[roomID];
+      if (room) {
+        room = room.filter((id) => id !== socket.id);
+        users[roomID] = room;
+      }
+      socket.broadcast.emit("user left", socket.id);
     });
-
-    app.post("/api/logout", (req, res) => {
-        userHandlers.logoutUser(db, req, res);
-    });
-
-    app.get("/api/profile/:id", (req, res) => {
-        userHandlers.getProfile(db, req, res, cache);
-    });
-    app.post("/api/changeProfile", (req, res) => {
-        userHandlers.changeProfile(db, req, res);
-    });
-    app.post("/api/sendMessage", (req, res) => {
-        messageHandlers.sendMessage(db, req, res);
-    });
-
-    app.post("/api/sendPicture", upload.single("image"), (req, res) => {
-        messageHandlers.sendPicture(db, req, res);
-    });
-    app.post("/api/sendFile", upload.single("file"), (req, res) => {
-        messageHandlers.sendFile(db, req, res);
-    });
-    app.post("/api/getMessages", (req, res) => {
-        messageHandlers.getMessages(db, req, res);
-    });
-    app.post("/api/deleteMessage", (req, res) => {
-        messageHandlers.deleteMessage(db, req, res);
-    });
-    app.get("/api/generalData", (req, res) => {
-        userHandlers.getGeneralData(db, req, res, cache);
-    });
-    //Start server
-    const server = app.listen(8080, () => {
-        console.log("started on 8080");
-    });
-    const io = socket(server, {
-        cors: {
-            origin: "*",
-        },
-    });
-
-    io.on("connection", (socket) => {
-        socket.on("join room", (roomID) => {
-            if (users[roomID]) {
-                const length = users[roomID].length;
-                if (length === 10) {
-                    socket.emit("room full");
-                    return;
-                }
-                users[roomID].push(socket.id);
-            } else {
-                users[roomID] = [socket.id];
-            }
-            socketToRoom[socket.id] = roomID;
-            const usersInThisRoom = users[roomID].filter(
-                (id) => id !== socket.id
-            );
-
-            socket.emit("all users", usersInThisRoom);
-        });
-
-        socket.on("sending signal", (payload) => {
-            io.to(payload.userToSignal).emit("user joined", {
-                signal: payload.signal,
-                callerID: payload.callerID,
-            });
-        });
-
-        socket.on("returning signal", (payload) => {
-            io.to(payload.callerID).emit("receiving returned signal", {
-                signal: payload.signal,
-                id: socket.id,
-            });
-        });
-
-        socket.on("disconnect", () => {
-            const roomID = socketToRoom[socket.id];
-            let room = users[roomID];
-            if (room) {
-                room = room.filter((id) => id !== socket.id);
-                users[roomID] = room;
-            }
-            socket.broadcast.emit("user left", socket.id);
-        });
-    });
+  });
 }
 
 main();
--- user/userHandlers.js
+++ user/userHandlers.js
@@ -5,165 +5,179 @@ const { ObjectID, Db } = require("mongodb");
 
 const privateKey = process.env.PRIVATE_KEY;
 
-function registerUser(db, req, res) {
-    let userData = req.body.userData;
-    db.collection("Users").findOne({ email: userData.email }, (err, data) => {
-        if (err) res.status(500).send({ error: "DB did not respond" });
-        else if (data == null) {
-            db.collection("Users").insertOne(
-                {
-                    email: userData.email,
-                    password: sha256.x2(userData.password),
-                    firstName: userData.firstName,
-                    lastName: userData.lastName,
-                    grade: userData.grade,
-                },
-                (err, data) => {
-                    console.log(err);
-                    console.log(data);
+function registerUser(db, req, res, mail) {
+  let userData = req.body.userData;
+  db.collection("Users").findOne({ email: userData.email }, (err, data) => {
+    if (err) res.status(500).send({ error: "DB did not respond" });
+    else if (data == null) {
+      db.collection("Users").insertOne(
+        {
+          email: userData.email,
+          password: sha256.x2(userData.password),
+          firstName: userData.firstName,
+          lastName: userData.lastName,
+          grade: userData.grade,
+          confirmed: false,
+        },
+        (err, data) => {
+          console.log(err);
+          console.log(data);
 
-                    if (err) {
-                        res.status(401).send({
-                            reason:
-                                "Sunteti deja inregistrat cu acest email in aplicatie!",
-                        });
-                    } else {
-                        var token = jwt.sign(
-                            { userID: ObjectID(data._id) },
-                            privateKey
-                        );
-
-                        res.status(200).send({
-                            response:
-                                "V-ati inregistrat cu succes! Va rugam verificati mailul pentru confirmare",
-                            jwt: token,
-                            grade: data.grade,
-                            firstName: data.firstName,
-                            lastName: data.lastName,
-                        });
-                    }
-                }
-            );
-        } else {
+          if (err) {
             res.status(401).send({
-                reason: "Sunteti deja inregistrat cu acest email in aplicatie!",
+              reason: "Sunteti deja inregistrat cu acest email in aplicatie!",
             });
+          } else {
+            var token = jwt.sign({ userID: ObjectID(data._id) }, privateKey);
+            var mailOptions = {
+              from: "studyroomsnoreply@gmail.com",
+              to: userData.email,
+              subject: "Registered to StudyRooms",
+              html: `<h1>This is your confirmation link </h1><a href="35.195.14.52:8080/api/confirm/${data.insertedId}">35.195.14.52:8080/api/confirm/${data.insertedId}</a>`,
+            };
+
+            mail.sendMail(mailOptions, function (error, info) {
+              if (error) {
+                console.log(error);
+              } else {
+                console.log("Email sent: " + info.response);
+              }
+            });
+            res.status(200).send({
+              response:
+                "V-ati inregistrat cu succes! Va rugam verificati mailul pentru confirmare",
+              jwt: token,
+              grade: data.grade,
+              firstName: data.firstName,
+              lastName: data.lastName,
+            });
+          }
         }
-    });
+      );
+    } else {
+      res.status(401).send({
+        reason: "Sunteti deja inregistrat cu acest email in aplicatie!",
+      });
+    }
+  });
 }
 
 function loginUser(db, req, res) {
-    let userData = req.body.userData;
-    console.log(userData);
-    db.collection("Users").findOne({ email: userData.email }, (err, data) => {
-        console.log(data);
-        if (data) {
-            if (err)
-                res.status(401).send({ reason: "Nu aveti cont in aplicatie" });
-            else if (sha256.x2(userData.password) === data.password) {
-                var token = jwt.sign(
-                    { userID: ObjectID(data._id) },
-                    privateKey
-                );
-                res.status(200).send({
-                    response: "V-ati autentificat cu succes!",
-                    jwt: token,
-                    grade: data.grade,
-                    firstName: data.firstName,
-                    lastName: data.lastName,
-                });
-            } else {
-                res.status(401).send({
-                    reason: "Mailul sau parola sunt gresite",
-                });
-            }
-        } else {
-            res.status(401).send({
-                reason: "Nu exista un cont cu acest email",
-            });
-        }
-    });
+  let userData = req.body.userData;
+  console.log(userData);
+  db.collection("Users").findOne({ email: userData.email }, (err, data) => {
+    console.log(data);
+    if (data && data.confirmed == true) {
+      if (err) res.status(401).send({ reason: "Nu aveti cont in aplicatie" });
+      else if (sha256.x2(userData.password) === data.password) {
+        var token = jwt.sign({ userID: ObjectID(data._id) }, privateKey);
+        res.status(200).send({
+          response: "V-ati autentificat cu succes!",
+          jwt: token,
+          grade: data.grade,
+          firstName: data.firstName,
+          lastName: data.lastName,
+        });
+      } else {
+        res.status(401).send({
+          reason: "Mailul sau parola sunt gresite",
+        });
+      }
+    } else {
+      res.status(401).send({
+        reason: "Nu exista un cont cu acest email sau nu ati validat emailul",
+      });
+    }
+  });
 }
 
 function getProfile(db, req, res, cache) {
-    let userID = req.params.id;
-    let userData = cache.get(req.params.id);
-    if (userData == undefined) {
-        db.collection("Users").findOne(
-            { _id: ObjectID(userID) },
-            (err, data) => {
-                if (err)
-                    res.status(401).send({
-                        reason: "No user exists with the specified ID",
-                    });
-                else if (data) {
-                    delete data.password;
-                    delete data.email;
-                    cache.set(userID, data, 10000);
-                    res.status(200).send(data);
-                } else {
-                    res.status(401).send("oops");
-                }
-            }
-        );
-    } else res.status(200).send(userData);
+  let userID = req.params.id;
+  let userData = cache.get(req.params.id);
+  if (userData == undefined) {
+    db.collection("Users").findOne({ _id: ObjectID(userID) }, (err, data) => {
+      if (err)
+        res.status(401).send({
+          reason: "No user exists with the specified ID",
+        });
+      else if (data) {
+        delete data.password;
+        delete data.email;
+        cache.set(userID, data, 10000);
+        res.status(200).send(data);
+      } else {
+        res.status(401).send("oops");
+      }
+    });
+  } else res.status(200).send(userData);
 }
 function changeProfile(db, req, res) {
-    let userID = req.body.userID;
-    let userJwt = req.body.jwt;
-    let changes = req.body.changes;
-    if (userID && userJwt) {
-        try {
-            jwtDecoded = jwt.verify(userJwt, privateKey);
-        } catch (e) {
-            res.status(401).send({ reason: "Jwt not valid" });
+  let userID = req.body.userID;
+  let userJwt = req.body.jwt;
+  let changes = req.body.changes;
+  if (userID && userJwt) {
+    try {
+      jwtDecoded = jwt.verify(userJwt, privateKey);
+    } catch (e) {
+      res.status(401).send({ reason: "Jwt not valid" });
+    }
+    if (jwtDecoded.userID == userID) {
+      console.log(changes);
+      db.collection("Users").updateOne(
+        { _id: ObjectID(userID) },
+        { $set: changes },
+        (err, data) => {
+          res.status(200).send("Changes done");
         }
-        if (jwtDecoded.userID == userID) {
-            console.log(changes);
-            db.collection("Users").updateOne(
-                { _id: ObjectID(userID) },
-                { $set: changes },
-                (err, data) => {
-                    res.status(200).send("Changes done");
-                }
-            );
-        }
-    } else res.status(401).send({ reason: "missing data" });
+      );
+    }
+  } else res.status(401).send({ reason: "missing data" });
 }
 function getGeneralData(db, req, res, cache) {
-    let generalData = cache.get("generalData");
-    console.log(generalData);
-    if (generalData == undefined) {
-        console.log("nice");
-        db.collection("Users")
-            .countDocuments({})
-            .then((data) => {
-                db.collection("messages")
-                    .countDocuments({})
-                    .then((msgdata) => {
-                        db.collection("Users")
-                            .findOne({}, { sort: { _id: -1 }, limit: 1 })
-                            .then((user) => {
-                                delete user.password;
-                                delete user.email;
-                                delete user._id;
-                                res.status(200).send({
-                                    users: data,
-                                    messages: msgdata,
-                                    lastUser: user,
-                                });
-                                cache.set("generalData", {
-                                    users: data,
-                                    messages: msgdata,
-                                    lastUser: user,
-                                });
-                            });
-                    });
-            });
-    } else res.status(200).send(generalData);
+  let generalData = cache.get("generalData");
+  console.log(generalData);
+  if (generalData == undefined) {
+    console.log("nice");
+    db.collection("Users")
+      .countDocuments({})
+      .then((data) => {
+        db.collection("messages")
+          .countDocuments({})
+          .then((msgdata) => {
+            db.collection("Users")
+              .findOne({}, { sort: { _id: -1 }, limit: 1 })
+              .then((user) => {
+                delete user.password;
+                delete user.email;
+                delete user._id;
+                res.status(200).send({
+                  users: data,
+                  messages: msgdata,
+                  lastUser: user,
+                });
+                cache.set("generalData", {
+                  users: data,
+                  messages: msgdata,
+                  lastUser: user,
+                });
+              });
+          });
+      });
+  } else res.status(200).send(generalData);
+}
+function confirmAccount(db, req, res) {
+  let id = req.params.id;
+  db.collection("Users").updateOne(
+    { _id: id },
+    { $set: { confirmed: true } },
+    (err, data) => {
+      res.status(200).send("Confirmed!");
+    }
+  );
 }
 exports.registerUser = registerUser;
 exports.loginUser = loginUser;
 exports.getProfile = getProfile;
 exports.getGeneralData = getGeneralData;
 exports.changeProfile = changeProfile;
+exports.confirmAccount = confirmAccount;
